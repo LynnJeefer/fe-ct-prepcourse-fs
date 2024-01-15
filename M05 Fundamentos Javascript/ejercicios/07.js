749c53449a9170489c64ee5cb37b461d2dd29c5a
LynnMeza <lynn770450@gmail.com>
2024-01-15 10:29:29 -0500

    fixed: Solutio

--- M05 Fundamentos Javascript/ejercicios/07.js
+++ M05 Fundamentos Javascript/ejercicios/07.js
@@ -3,6 +3,10 @@ function esTipoDato(valor) {
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
+  var tipo= typeof valor
+  return tipo 
 }
+const valor=42
 
+console.log(esTipoDato(valor))
 module.exports = esTipoDato;
